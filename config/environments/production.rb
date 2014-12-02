b66ba690dab60607755874e9dd7ea2a50fdbac67
tarnelope <teresatarn@gmail.com>
2014-12-02 15:57:04 -0700

    changed serve static assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ HITWebsite::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Changed to true on 12/2/2014
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
